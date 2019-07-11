ff48067f88d9d36662090a80a922a9fd0a9354ae
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-11 23:49:00 +0430

    Bump lodash for the latest security patch

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,8 @@ import Wrapper from '../components/Wrapper'
 export default function Index() {
 	return (
 		  <Wrapper>
-			<h1>Welcome to NextJS boilerplate</h1>
-			<a href="https://github.com/nikandlv/nextjs-boilerplate">Github repo</a>
+			  <h1>Welcome to NextJS boilerplate</h1>
+			  <a href="https://github.com/nikandlv/iran-react">Github repo</a>
 		  </Wrapper>
-
 	);
 }
