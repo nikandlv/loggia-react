631d1e8ff9481196d51fd0c037719ce09441f186
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-14 23:17:08 +0430

    Added expand Icon

--- components/BottomBar/index.jsx
+++ components/BottomBar/index.jsx
@@ -19,6 +19,9 @@ export class BottomBar extends React.Component {
                         <PhotoIndicator number={key} key={key} title={item} active={key === this.state.active} />
                     )
                 })}
+                <div className="expand-icon-wrapper">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path className="icon" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+                </div>
             </section>
         )
     }
