e4aa28892b236293f082c0a8bd26f7d63536209f
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-16 01:06:29 +0430

    Added proper animation and 3200ms auto loop

--- components/Gallery/PhotoZone.jsx
+++ components/Gallery/PhotoZone.jsx
@@ -4,21 +4,43 @@ export default class PhotoZone extends React.Component {
     constructor(props) {
         super(props)
         this.view = React.createRef();
+        this.transition = false
     }
     state = {
-        loading:true,
+        current:{title:"",photo:""},
+    }
+    componentDidMount() {
+        this.updateItem()
     }
     updateItem = () => {
         window.setTimeout(()=> {
             this.view.current.className = this.view.current.className.replace("loading","loaded")
-        },500)
-    }
-    componentDidUpdate(props,state) {
-        if(this.props.active !== this.state.active) {
+            this.transition = false
+            this.forceUpdate()
             window.setTimeout(()=> {
-                this.view.current.className = this.view.current.className.replace("loaded","loading")
-            },500)
+                let active = this.props.active
+                if(active === 9) {
+                    active = 0
+                } else {
+                    active += 1
+                }
+                this.props.setItem(active)
+            },3200)
+        },370)
+    }
+    shouldComponentUpdate() {
+        console.log(this.transition)
+        if(this.transition === true) {
+            this.transition = false
+            return true    
         }
+        console.log("loading")
+        this.view.current.className = this.view.current.className.replace("loaded","loading")
+        window.setTimeout(()=> {
+            this.transition = true
+            this.forceUpdate()
+        },370)
+        return false
     }
     render() {
         const {items,active} = this.props;
@@ -26,7 +48,7 @@ export default class PhotoZone extends React.Component {
         return (
             <div className="zone">
                 <img alt="image" src={item.photo} onLoad={this.updateItem} style={{display:'none'}} />
-                <div className={`view-1 active loading`} ref={this.view} style={{backgroundImage:`url(${item.photo})`}}>
+                <div className={`view active loaded`} ref={this.view} style={{backgroundImage:`url(${item.photo})`}}>
                 </div>
                 <div className="overlay" ref={this.overlay} />
             </div>
