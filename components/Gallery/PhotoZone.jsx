69a261794600f6835f0eb4a52d6c9ff42d0efa9e
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-15 23:54:38 +0430

    Moved view to ref

--- components/Gallery/PhotoZone.jsx
+++ components/Gallery/PhotoZone.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
 export default class PhotoZone extends React.Component {
+    constructor(props) {
+        super(props)
+        this.view = React.createRef();
+    }
     state = {
         loading:true,
     }
     updateItem = () => {
         
-    
     }
     componentDidUpdate(props,state) {
-
         if(this.props.active !== this.state.active) {
-            console.log("update")
+            console.log("update",this.view.current)
         }
     }
     render() {
@@ -22,7 +24,7 @@ export default class PhotoZone extends React.Component {
         return (
             <div className="zone">
                 <img alt="image" src={item.photo} onLoad={this.updateItem} style={{display:'none'}} />
-                <div className={`view-1 active ${status}`} ref={this.view1} style={{backgroundImage:`url(${item.photo})`}}>
+                <div className={`view-1 active ${status}`} ref={this.view} style={{backgroundImage:`url(${item.photo})`}}>
                 </div>
                 <div className="overlay" ref={this.overlay} />
             </div>
