312561e564edb372a52eb3e0bbb43cd4f92da2ae
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-16 23:23:11 +0430

    Added a basic progress bar

--- components/Gallery/PhotoZone.jsx
+++ components/Gallery/PhotoZone.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 export default class PhotoZone extends React.Component {
     constructor(props) {
         super(props)
@@ -8,19 +7,44 @@ export default class PhotoZone extends React.Component {
     }
     state = {
         current:{title:"",photo:""},
+        progress:0
     }
     componentDidMount() {
         this.updateItem()
     }
+    progressTimeout = {};
+    loopProgress = () => {
+        this.progressTimeout = window.setTimeout(()=> {
+            if(this.state.progress === 100) {
+                return
+            }
+            this.isProgress = true
+            this.setState({
+                progress:this.state.progress+1
+            })
+            this.loopProgress()
+        },(3000/100))
+    }
+    isProgress = false
     updateItem = () => {
         window.setTimeout(()=> {
             this.view.current.className = this.view.current.className.replace("loading","loaded")
             this.transition = false
             this.forceUpdate()
             this.props.continueLoop()
+            this.isProgress = true
+            this.setState({
+                progress:0
+            })
+            clearTimeout(this.progressTimeout);
+            this.loopProgress()
         },370)
     }
     shouldComponentUpdate() {
+        if(this.isProgress) {
+            this.isProgress = false
+            return true
+        }
         if(this.transition === true) {
             this.transition = false
             return true    
@@ -36,11 +60,14 @@ export default class PhotoZone extends React.Component {
         const {items,active} = this.props;
         const item = items[active];
         return (
-            <div className="zone">
-                <img alt="image" src={item.photo} onLoad={this.updateItem} style={{display:'none'}} />
-                <div className={`view active loaded`} ref={this.view} style={{backgroundImage:`url(${item.photo})`}}>
+            <div>
+                <div className="zone">
+                    <img alt="image" src={item.photo} onLoad={this.updateItem} style={{display:'none'}} />
+                    <div className={`view active loaded`} ref={this.view} style={{backgroundImage:`url(${item.photo})`}}>
+                    </div>
+                    <div className="overlay" ref={this.overlay} />
                 </div>
-                <div className="overlay" ref={this.overlay} />
+                <div className="progress" style={{width:this.state.progress+"%"}}/>
             </div>
         )
     }
--- pages/index.jsx
+++ pages/index.jsx
@@ -73,7 +73,7 @@ export default class index extends React.Component {
 		this.setState({
 			active
 		})
-		clearTimeout(this.timeOut);
+		clearTimeout(this.timeOut );
     }
 	render() {
 		return (
