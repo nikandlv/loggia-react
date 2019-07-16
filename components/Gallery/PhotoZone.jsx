23b36c89d555b1da10d8f01cd919c4396845c771
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-16 23:33:35 +0430

    Moved progressbar into css animations for easier access and better performance

--- components/Gallery/PhotoZone.jsx
+++ components/Gallery/PhotoZone.jsx
@@ -3,53 +3,31 @@ export default class PhotoZone extends React.Component {
     constructor(props) {
         super(props)
         this.view = React.createRef();
+        this.progress = React.createRef();
         this.transition = false
     }
     state = {
         current:{title:"",photo:""},
-        progress:0
     }
     componentDidMount() {
         this.updateItem()
     }
-    progressTimeout = {};
-    loopProgress = () => {
-        this.progressTimeout = window.setTimeout(()=> {
-            if(this.state.progress === 100) {
-                return
-            }
-            this.isProgress = true
-            this.setState({
-                progress:this.state.progress+1
-            })
-            this.loopProgress()
-        },(3000/100))
-    }
-    isProgress = false
     updateItem = () => {
         window.setTimeout(()=> {
             this.view.current.className = this.view.current.className.replace("loading","loaded")
+            this.progress.current.className = this.progress.current.className.replace("loading","loaded")
             this.transition = false
             this.forceUpdate()
             this.props.continueLoop()
-            this.isProgress = true
-            this.setState({
-                progress:0
-            })
-            clearTimeout(this.progressTimeout);
-            this.loopProgress()
         },370)
     }
     shouldComponentUpdate() {
-        if(this.isProgress) {
-            this.isProgress = false
-            return true
-        }
         if(this.transition === true) {
             this.transition = false
             return true    
         }
         this.view.current.className = this.view.current.className.replace("loaded","loading")
+        this.progress.current.className = this.progress.current.className.replace("loaded","loading")
         window.setTimeout(()=> {
             this.transition = true
             this.forceUpdate()
@@ -67,7 +45,7 @@ export default class PhotoZone extends React.Component {
                     </div>
                     <div className="overlay" ref={this.overlay} />
                 </div>
-                <div className="progress" style={{width:this.state.progress+"%"}}/>
+                <div className="progress loading" ref={this.progress} />
             </div>
         )
     }
