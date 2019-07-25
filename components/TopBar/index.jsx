d0116b57e451adec902373117ec9f9c165cafb3d
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-25 21:55:07 +0430

    Changed info button to location button

--- components/TopBar/index.jsx
+++ components/TopBar/index.jsx
@@ -5,7 +5,7 @@ export default function TopBar(props) {
         <div className={`top-bar ${(activeItem.top_light?' light':'')}`}>
             <div className="push">
                 <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path className="icon" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path class="icon" d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
                 </div>
             </div>
             <div>
