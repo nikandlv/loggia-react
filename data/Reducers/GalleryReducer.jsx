75addd0de620acd792bb062bad629bf602a1d246
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-19 23:53:24 +0430

    Added more state to the reducer

--- data/Reducers/GalleryReducer.jsx
+++ data/Reducers/GalleryReducer.jsx
@@ -1,8 +1,12 @@
 import * as types from '../Actions/types'
+import Items from '../resources/items.json'
 
 const initialState = {
     fullscreen: false,
-    language: 'en'
+    language: 'en',
+    active: 0,
+    photos: Items,
+    overview: false
 }
 
 export default function GalleryReducer(state = initialState,action) {
