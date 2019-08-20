55e7f301231befca3e2281bcb1dde07bf78848ac
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-20 22:40:40 +0430

    Cleared gallery component

--- components/Gallery/index.jsx
+++ components/Gallery/index.jsx
@@ -4,18 +4,8 @@ export class Gallery extends React.Component {
     render() {
         return (
             <section className="gallery">
-                <PhotoZone fullscreen={this.props.fullscreen} items={this.props.items} active={this.props.active} setItem={this.props.setItem} continueLoop={this.props.continueLoop} />
+                <PhotoZone />
             </section>
         )
-    }
-    shouldComponentUpdate(props) {
-        let newActive = props.active
-        let oldActive = this.props.active
-        let newLanguage = props.language
-        let oldLanguage = this.props.language
-        if(oldActive === newActive && oldLanguage === newLanguage) {
-            return false
-        }
-        return true
     }
 }
